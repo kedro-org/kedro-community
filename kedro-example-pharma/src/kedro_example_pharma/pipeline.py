940151bd008aac2a9b9e609a4da99e7981dfee34
Robert Muil <Robert_Muil@mckinsey.com>
2019-11-25 12:36:16 +0000

    neatened up layers in tags

--- kedro-example-pharma/src/kedro_example_pharma/pipeline.py
+++ kedro-example-pharma/src/kedro_example_pharma/pipeline.py
@@ -28,11 +28,10 @@
 """Pipeline construction."""
 
 from kedro.pipeline import Pipeline, node
+from kedro.pipeline.decorators import log_time
 from kedro_example_pharma.nodes.data_engineering import (
     preprocess_observations,
     preprocess_diagnosis,
-    create_master_table,
-    log_running_time,
 )
 
 from kedro_example_pharma.nodes.price_prediction import (
@@ -102,86 +101,87 @@ def create_pipeline(**kwargs):
                 preprocess_observations,
                 "raw: observations",
                 "int: typed_observations",
-                tags=["observations", "master", "raw"]
+                tags=["observations", "1 - raw"]
             ),
             node(
                 preprocess_diagnosis,
                 "raw: diagnosis",
                 "int: typed_diagnosis",
-                tags=["diagnosis", "master", "raw"]
+                tags=["diagnosis", "1 - raw"]
             ),
             node(
                 preprocess_labs,
                 "raw: lab_values",
                 "int: typed_lab_values",
-                tags=["labs", "master", "raw"]
+                tags=["labs", "1 - raw"]
             ),
             node(
                 preprocess_labs,
                 "raw: lab_ranges",
                 "int: typed_lab_ranges",
-                tags=["labs", "master", "raw"]
+                tags=["labs", "1 - raw"]
             ),
             node(
                 pass_through,
                 "raw: patients",
                 "int: patients",
-                tags=["patients", "master", "raw"]
+                tags=["patients", "1 - raw"]
             ),
             node(
                 pass_through,
                 "int: patients",
                 "prm: patients",
-                tags=["patients", "master", "raw"]
+                tags=["patients", "2 - intermediate"]
             ),
             node(
                 transform_observations,
                 "int: typed_observations",
                 "prm: patient_indexed_observations",
-                tags=["observations", "master", "int"]
+                tags=["observations", "2 - intermediate"]
             ),
             node(
                 pass_through,
                 "int: typed_diagnosis",
                 "prm: diagnosis",
-                tags=["diagnosis", "master", "int"]
+                tags=["diagnosis", "2 - intermediate"]
             ),
             node(
                 combine_lab_values_and_ranges,
                 ["int: typed_lab_ranges", "int: typed_lab_values"],
                 "prm: labs",
-                tags=["labs", "master", "int"]
+                tags=["labs", "2 - intermediate"]
             ),
             node(
                 create_endpoint_stomach_cancer,
                 ["prm: patient_indexed_observations", "prm: diagnosis", "prm: labs"],
                 "fea: endpoint_stomach_cancer",
-                tags=["master", "prm", "endpoints"]
+                tags=["3 - primary", "endpoints"]
 
             ),
             node(
                 create_demographics,
                 ["prm: patients"],
                 "fea: demographics",
-                tags=["master", "prm", "features"]
+                tags=["3 - primary", "features"]
             ),
             node(
                 create_model_input_stomach_cancer,
                 ["fea: endpoint_stomach_cancer", "fea: demographics"],
-                "model_input: cancer_incidence",
-                tags=["master", "model_input"]
+                "inp: cancer_incidence",
+                tags=["4 - feature"]
 
             ),
         ],
         tags=["de"]
-    ).decorate(log_running_time)
+    ).decorate(log_time)
 
     ds_pipeline = Pipeline(
         [
             node(
                 split_data,
-                ["model_input: cancer_incidence", "param: train_split_proportion"],
+                ["inp: cancer_incidence", "param: train_split_proportion"],
                 ["X_train", "X_test", "y_train", "y_test"],
+                tags=["5 - model input"]
             ),
             node(
                 train_cox_ph_model,
@@ -195,6 +195,6 @@ def create_pipeline(**kwargs):
             ),
         ],
         tags=["ds"],
-    ).decorate(log_running_time)
+    ).decorate(log_time)
 
     return de_pipeline + ds_pipeline
