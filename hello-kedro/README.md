7919f9925d7c0bd7e17a298feed35459d30a0c93
Jo Stichbury <jo.stichbury@mac.com>
2020-06-19 15:52:56 +0100

    Add "Hello world" example

---
+++ hello-kedro/README.md
@@ -0,0 +1,7 @@
+# Hello Kedro Example
+
+This is the example code described in the [Hello Kedro](https://github.com/quantumblacklabs/private-kedro/blob/develop/docs/source/02_getting_started/04_hello_kedro.md) documentation. 
+
+<!-- NOTE: LINK TO CHANGE SHORTLY -->
+
+As is traditional for "Hello World" projects, it is a very simplistic example to introduce the most basic elements of a Kedro project within a small amount of code.
---
+++ hello-kedro/hello-kedro.py
@@ -0,0 +1,34 @@
+
+from kedro.io import DataCatalog, MemoryDataSet
+from kedro.pipeline import node, Pipeline
+from kedro.runner import SequentialRunner
+
+# Prepare a data catalog
+data_catalog = DataCatalog({
+    "example_data": MemoryDataSet()
+})
+
+
+# Prepare nodes
+def return_greeting():
+    return "Hello"
+
+return_greeting_node = node(return_greeting, inputs=None, outputs="my_salutation")
+
+def join_statements(greeting):
+    return f"{greeting} Kedro!"
+
+join_statements_node = node(join_statements, inputs="my_salutation", outputs="my_message")
+
+
+# Assemble nodes into a pipeline
+pipeline = Pipeline([
+    return_greeting_node, join_statements_node
+])
+
+
+# Create a runner
+runner = SequentialRunner()
+
+# Run the pipeline
+runner.run(pipeline, data_catalog)
