cd7189417fccacbd8765fd3ca85e972253d444b3
Jo Stichbury <stichbury@users.noreply.github.com>
2020-06-23 17:36:46 +0100

    add pythonic formatting (#49)
    Used 'black' library to format the code in a pythonic manner
    
    Co-authored-by: Lais Carvalho <lais.carvalho@python.ie>

--- hello-kedro/hello-kedro.py
+++ hello-kedro/hello-kedro.py
@@ -1,31 +1,32 @@
-
 from kedro.io import DataCatalog, MemoryDataSet
 from kedro.pipeline import node, Pipeline
 from kedro.runner import SequentialRunner
 
 # Prepare a data catalog
-data_catalog = DataCatalog({
-    "example_data": MemoryDataSet()
-})
+data_catalog = DataCatalog({"example_data": MemoryDataSet()})
 
 
-# Prepare nodes
 def return_greeting():
+    # Prepare first node
     return "Hello"
 
-return_greeting_node = node(return_greeting, inputs=None, outputs="my_salutation")
+
+return_greeting_node = node(
+    return_greeting, inputs=None, outputs="my_salutation"
+)
+
 
 def join_statements(greeting):
+    # Prepare second node
     return f"{greeting} Kedro!"
 
-join_statements_node = node(join_statements, inputs="my_salutation", outputs="my_message")
 
+join_statements_node = node(
+    join_statements, inputs="my_salutation", outputs="my_message"
+)
 
 # Assemble nodes into a pipeline
-pipeline = Pipeline([
-    return_greeting_node, join_statements_node
-])
-
+pipeline = Pipeline([return_greeting_node, join_statements_node])
 
 # Create a runner
 runner = SequentialRunner()
