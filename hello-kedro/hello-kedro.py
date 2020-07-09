de8e8502632f997c8225f5768791ece2b0b2de83
Jo Stichbury <stichbury@users.noreply.github.com>
2020-07-09 17:47:06 +0100

    Update hello-kedro.py

--- hello-kedro/hello-kedro.py
+++ hello-kedro/hello-kedro.py
@@ -13,15 +13,11 @@ def return_greeting():
 
 return_greeting_node = node(return_greeting, inputs=None, outputs="my_salutation")
 
-
 def join_statements(greeting):
     # Prepare second node
     return f"{greeting} Kedro!"
 
-
-join_statements_node = node(
-    join_statements, inputs="my_salutation", outputs="my_message"
-)
+join_statements_node = node(join_statements, inputs="my_salutation", outputs="my_message")
 
 # Assemble nodes into a pipeline
 pipeline = Pipeline([return_greeting_node, join_statements_node])
