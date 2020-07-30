e078575a211545921ba3d94b281d6d69c1b3ff6e
Yetunde Dada <43755008+yetudada@users.noreply.github.com>
2020-07-30 19:57:04 +0100

    Update hello-kedro.py
    - Moved comments
    - Added in print statement

--- hello-kedro/hello-kedro.py
+++ hello-kedro/hello-kedro.py
@@ -1,3 +1,4 @@
+"""Contents of hello_kedro.py"""
 from kedro.io import DataCatalog, MemoryDataSet
 from kedro.pipeline import node, Pipeline
 from kedro.runner import SequentialRunner
@@ -5,25 +6,29 @@ from kedro.runner import SequentialRunner
 # Prepare a data catalog
 data_catalog = DataCatalog({"example_data": MemoryDataSet()})
 
-
+# Prepare first node
 def return_greeting():
-    # Prepare first node
     return "Hello"
 
 
-return_greeting_node = node(return_greeting, inputs=None, outputs="my_salutation")
+return_greeting_node = node(
+    return_greeting, inputs=None, outputs="my_salutation"
+)
 
+# Prepare second node
 def join_statements(greeting):
-    # Prepare second node
     return f"{greeting} Kedro!"
 
-join_statements_node = node(join_statements, inputs="my_salutation", outputs="my_message")
+
+join_statements_node = node(
+    join_statements, inputs="my_salutation", outputs="my_message"
+)
 
 # Assemble nodes into a pipeline
 pipeline = Pipeline([return_greeting_node, join_statements_node])
 
-# Create a runner
+# Create a runner to run the pipeline
 runner = SequentialRunner()
 
 # Run the pipeline
-runner.run(pipeline, data_catalog)
+print(runner.run(pipeline, data_catalog))
